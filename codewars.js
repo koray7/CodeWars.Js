f8f59b9e747112a10310c523f72f033c83d10a01
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-09-04 16:19:38 -0500

    Challanges

---
+++ codewars.js
@@ -0,0 +1,12 @@
+// Find non-consecutive number
+
+// function nonConsecutive(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     const curr = arr[i];
+//     const next = arr[i + 1];
+//     if (curr + 1 !== next) {
+//       return next;
+//     }
+//   }
+//   return null;
+// }
