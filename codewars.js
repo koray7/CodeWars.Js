c222af84a73c9be9a308f5ba6530dc89eb46121c
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-09-10 13:09:04 -0500

    CodeWar Algo Questions

--- codewars.js
+++ codewars.js
@@ -10,3 +10,11 @@
 //   }
 //   return null;
 // }
+
+// Given an array of integers your solution should find the smallest integer.
+
+// class SmallestIntegerFinder {
+//   findSmallestInt(args) {
+//     return Math.min(...args);
+//   }
+// }
